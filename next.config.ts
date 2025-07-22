9fabdc77cdc6c7f1d66533c87ac000b864191b39
wamin suwito <waminsuwito1@gmail.com>
2025-07-22 15:48:26 +0000

    kodenya masih sama dengan yang sebelumnya

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
