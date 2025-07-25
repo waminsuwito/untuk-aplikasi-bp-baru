95795e785c2374eca2bbbf135dfa5c388773f61e
wamin suwito <waminsuwito1@gmail.com>
2025-07-25 04:16:46 +0000

    coba cek lagi masih ada database firebase yang tersisa tidak

--- src/app/admin-bp/print-job-listener.tsx
+++
@@ -1,7 +0,0 @@
-
-'use client';
-// This component is disabled because it relies on a Firebase Realtime Database connection,
-// which has been removed from the project.
-export function PrintJobListener() {
-  return null;
-}
